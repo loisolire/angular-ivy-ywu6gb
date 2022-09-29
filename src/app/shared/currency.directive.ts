e15bd300f86ff3735cabea65a205f1e573ca488c
loisolire <jean-marc.puechavy@vidal.fr>
2022-09-29 15:42:46 +0200

    2 files modified

--- src/app/shared/currency.directive.ts
+++ src/app/shared/currency.directive.ts
@@ -1,10 +1,12 @@
+import { CurrencyPipe } from '@angular/common';
 import { Directive, ElementRef } from '@angular/core';
 
 @Directive({
   selector: '[appCurrency]',
 })
 export class CurrencyDirective {
-  constructor(private el: ElementRef) {
-    el.nativeElement.style.color = 'red';
+  constructor(private el: ElementRef, currencyPipe: CurrencyPipe) {
+    let innerText = el.nativeElement.innerText;
+    innerText = currencyPipe.transform(innerText, 'USD', true);
   }
 }
