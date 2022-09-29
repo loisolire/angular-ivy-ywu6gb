08744b98b99eded9ac7f2cc60ab4d13fdf4fe18f
loisolire <jean-marc.puechavy@vidal.fr>
2022-09-29 16:44:12 +0200

    1 file modified

--- src/app/stock-panel-list/stock-panel-list.component.ts
+++ src/app/stock-panel-list/stock-panel-list.component.ts
@@ -2,14 +2,11 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-stock-panel-list',
-  templateUrl: './stock-panel-list.component.html',
-  styleUrls: ['./stock-panel-list.component.css']
+  template: '<p></p>',
+  styleUrls: ['./stock-panel-list.component.css'],
 })
 export class StockPanelListComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
